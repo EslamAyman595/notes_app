bc1a73ab01e31ab94e5b8b17a14f1e3a719b009a
Eslam Ayman <eslamaimn595@gmail.com>
2024-02-01 22:03:07 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,8 @@
+class NoteModel{
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  const NoteModel({required this.date, required this.color, required this.title, required this.subtitle});
+}
