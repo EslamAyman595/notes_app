3b82700c9c7732bac9d7f169ffb8b4757f5c7431
Eslam Ayman <eslamaimn595@gmail.com>
2024-02-09 21:29:07 +0200

    create fetch notes method

--- lib/cubits/notes_cubit/cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/cubit/notes_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constamt.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes()async{
+     emit(NotesLoading());
+ try {
+  var notesBox =Hive.box<NoteModel>(kNoteBox);
+  
+  List<NoteModel> notes  =notesBox.values.toList();
+  emit(NotesSuccess(notes));
+}  catch (e) {
+  //emit( AddNoteFaliure( e.toString()));
+  emit(NotesFaliure( e.toString()));
+}
+
+  }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constamt.dart';
+import 'package:notes_app/cubits/notes_cubit/cubit/notes_cubit.dart';
 import 'package:notes_app/widgets/addN_note_bottomsheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
@@ -8,30 +10,34 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: Container(
-        height: 50,
-        width: 50,
-        child: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-              isScrollControlled: true,//ينفع يتعمل لة scroll لو طلع ليا فوق شويه فى حاله ظهور الكيبورد مثلا فا bottom sheet كلو يطلع لفوق
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                context: context,
-                builder: (context) {
-                  return const AddNNoteBottomsheet();
-                });
-          },
-          child: Icon(Icons.add),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(60),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: Container(
+          height: 50,
+          width: 50,
+          child: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  isScrollControlled:
+                      true, //ينفع يتعمل لة scroll لو طلع ليا فوق شويه فى حاله ظهور الكيبورد مثلا فا bottom sheet كلو يطلع لفوق
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return const AddNNoteBottomsheet();
+                  });
+            },
+            child: Icon(Icons.add),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(60),
+            ),
+            backgroundColor: kprimarycolor,
           ),
-          backgroundColor: kprimarycolor,
         ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
